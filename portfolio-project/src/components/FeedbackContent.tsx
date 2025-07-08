713a8f88647e12234d386d81d69abd39ea68ecba
imnamgi <happynj2697@gmail.com>
2025-07-08 17:29:43 +0900

    Feat: feedback 타입정의 #2-04

--- portfolio-project/src/components/FeedbackContent.tsx
+++ portfolio-project/src/components/FeedbackContent.tsx
@@ -1,32 +1,16 @@
 'use client';
 
+import { RatingType, initialFeedback, FeedbackContentProps, FeedbackData } from '@/entities/feedback/model/types';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
-interface FeedbackContentProps {
-  isModal?: boolean;
-}
-
-const initialFeedback = {
-  ratings: {
-    portfolioScore: 5,
-    technicalScore: 5,
-    designScore: 5,
-    average_score: 5,
-  },
-  comment_text: '',
-  bug_description: '',
-  compony_name: '',
-  job_link: '',
-};
-
 export function FeedbackMain() {
-  const [feedback, setFeedback] = useState(() => {
+  const [feedback, setFeedback] = useState<FeedbackData>(() => {
     const temp = localStorage.getItem('feedback');
     return temp ? JSON.parse(temp) : initialFeedback;
   });
 
-  const updateScore = (type: string, num: number) => {
+  const updateScore = (type: RatingType, num: number) => {
     const newF = {
       ...feedback,
       ratings: {
@@ -41,29 +25,31 @@ export function FeedbackMain() {
   return (
     <>
       <div className="bg-foreground">
-        <div className="text-background">완성도</div>
-        <div className="space-x-1">
-          {Array.from({ length: 5 }, (_, i) => i + 1).map((score) => (
-            <button key={score} className={`${feedback.ratings.portfolioScore === score ? 'bg-rose-500' : 'bg-rose-300'} w-6 text-background`} onClick={() => updateScore('portfolioScore', score)}>
-              {score}
-            </button>
-          ))}
-        </div>
-        <div className="text-background">기술점수</div>
-        <div className="space-x-1">
-          {Array.from({ length: 5 }, (_, i) => i + 1).map((score) => (
-            <button key={score} className={`${feedback.ratings.technicalScore === score ? 'bg-rose-500' : 'bg-rose-300'} w-6 text-background`} onClick={() => updateScore('technicalScore', score)}>
-              {score}
-            </button>
-          ))}
-        </div>
-        <div className="text-background">디자인점수</div>
-        <div className="space-x-1">
-          {Array.from({ length: 5 }, (_, i) => i + 1).map((score) => (
-            <button key={score} className={`${feedback.ratings.designScore === score ? 'bg-rose-500' : 'bg-rose-300'} w-6 text-background`} onClick={() => updateScore('designScore', score)}>
-              {score}
-            </button>
-          ))}
+        <div>
+          <div className="text-background">완성도</div>
+          <div className="space-x-1">
+            {Array.from({ length: 5 }, (_, i) => i + 1).map((score) => (
+              <button key={score} className={`${feedback.ratings.portfolioScore === score ? 'bg-rose-500' : 'bg-rose-300'} w-6 text-background`} onClick={() => updateScore('portfolioScore', score)}>
+                {score}
+              </button>
+            ))}
+          </div>
+          <div className="text-background">기술점수</div>
+          <div className="space-x-1">
+            {Array.from({ length: 5 }, (_, i) => i + 1).map((score) => (
+              <button key={score} className={`${feedback.ratings.technicalScore === score ? 'bg-rose-500' : 'bg-rose-300'} w-6 text-background`} onClick={() => updateScore('technicalScore', score)}>
+                {score}
+              </button>
+            ))}
+          </div>
+          <div className="text-background">디자인점수</div>
+          <div className="space-x-1">
+            {Array.from({ length: 5 }, (_, i) => i + 1).map((score) => (
+              <button key={score} className={`${feedback.ratings.designScore === score ? 'bg-rose-500' : 'bg-rose-300'} w-6 text-background`} onClick={() => updateScore('designScore', score)}>
+                {score}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </>
