691200bb9ca6c53d14954e2538dc09d4a80de2db
imnamgi <happynj2697@gmail.com>
2025-07-09 16:01:42 +0900

    fix: 테스트파일 삭제

--- portfolio-project/src/app/test-for-env/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function TestForEnv() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const temp = process.env.NEXT_PUBLIC_WOO;
-  console.log(temp);
-
-  return <div>Hello World</div>;
-}
