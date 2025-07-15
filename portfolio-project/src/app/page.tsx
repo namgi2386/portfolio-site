64a2fc8603e7e6704a0829caf44d503e134efb80
imnamgi <happynj2697@gmail.com>
2025-07-15 13:44:33 +0900

    Modify: 피드백토글버튼 상태관리 수정

--- portfolio-project/src/app/page.tsx
+++ portfolio-project/src/app/page.tsx
@@ -1,11 +1,15 @@
+import ThemeToggle from '@/shared/ui/ThemeToggle';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <>
-      <div className="mt-8">
-        <Link href="/feedback" className="bg-rose-500 text-white px-4 py-2 rounded hover:bg-rose-600 inline-block">
-          피드백 모달 열기
+      <div className="fixed top-1/2 right-0 gap-4 h-60 bg-no2 w-20 flex flex-col justify-start p-2">
+        <div className="flex justify-center">
+          <ThemeToggle />
+        </div>
+        <Link href="/feedback" className="bg-rose-500 text-center h-10 w-16 flex items-center p-2 rounded hover:bg-rose-600 inline-block">
+          <div>피드백</div>
         </Link>
       </div>
     </>
--- portfolio-project/src/shared/ui/ThemeToggle.tsx
+++ portfolio-project/src/shared/ui/ThemeToggle.tsx
@@ -1,18 +1,18 @@
 'use client';
 import { useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { BsBrightnessAltHigh, BsBrightnessAltHighFill } from 'react-icons/bs';
 
 export default function ThemeToggle() {
-  const [mounted, setMounted] = useState(false);
+  // const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-  if (!mounted) return null;
+  // useEffect(() => {
+  //   setMounted(true);
+  // }, []);
   return (
     <button onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')} className="text-2xl">
-      {theme === 'dark' ? <BsBrightnessAltHighFill /> : <BsBrightnessAltHigh />}
+      <BsBrightnessAltHigh className="dark:hidden" />
+      <BsBrightnessAltHighFill className="hidden dark:block" />
     </button>
   );
 }
