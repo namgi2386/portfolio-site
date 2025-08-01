949e5826798e824f7320637445547ab25a763153
imnamgi <happynj2697@gmail.com>
2025-08-01 15:01:47 +0900

    Design: topbar 모바일사이즈 메뉴 스크롤제거

--- portfolio-project/src/shared/ui/MobileMenu.tsx
+++ portfolio-project/src/shared/ui/MobileMenu.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { IoClose } from 'react-icons/io5';
 import Link from 'next/link';
@@ -19,9 +19,19 @@ export default function MobileMenu() {
     setIsMenuOpen(false);
   };
 
+  useEffect(() => {
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'unset';
+    }
+    return () => {
+      document.body.style.overflow = 'unset';
+    };
+  }, [isMenuOpen]);
+
   return (
     <>
-      {/* 햄버거/닫기 버튼 */}
       <div className="flex md:hidden items-center">
         <button onClick={toggleMenu} className={ColorStyle}>
           {isMenuOpen ? <IoClose size={28} /> : <RxHamburgerMenu size={24} />}
@@ -29,7 +39,6 @@ export default function MobileMenu() {
       </div>
       {isMenuOpen && (
         <div className="fixed inset-0 -z-10 bg-background md:hidden pt-16">
-          {/* 메뉴 링크들 */}
           <div className="flex flex-col justify-center items-center h-full space-y-8 font-paperlogy">
             <Link href="/#profile-section" className={`text-3xl font-medium ${ColorStyle}`} onClick={closeMenu}>
               Profile
